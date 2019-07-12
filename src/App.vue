8f2f22297557229f2a3c083aed683633dbf4ad1d
Paweł Kieryk <pawel.kieryk@codewise.com>
2019-07-12 19:16:59 +0200

    Add app-container class, add Form component

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,28 @@
 <template>
     <div id="app">
         <AppHeader headline="New event"/>
+        <main class="app-container">
+            <Form @submit="submitForm">
+
+            </Form>
+        </main>
     </div>
 </template>
 
 <script>
 import AppHeader from '@/components/AppHeader.vue';
+import Form from '@/components/Form.vue';
 
 export default {
+    data: () => ({
+
+    }),
     components: {
-        AppHeader
+        AppHeader,
+        Form
+    },
+    methods: {
+
     }
 }
 </script>
---
+++ src/components/Form.vue
@@ -0,0 +1,11 @@
+<template>
+    <form class="form" @submit.prevent="$emit('submit')">
+        <slot></slot>
+    </form>
+</template>
+
+<script>
+export default {
+
+}
+</script>
